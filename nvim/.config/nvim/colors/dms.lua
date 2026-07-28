61415fb3522a203137599019b4f60a16c218a58c
maia-kitty <martin.cudek.ml@gmail.com>
2026-07-28 10:50:17 +0200

    added a bind to open zen browser and enabled cursor trails in kitty

--- nvim/.config/nvim/colors/dms.lua
+++ nvim/.config/nvim/colors/dms.lua
@@ -82,9 +82,9 @@ end
 
 if not base46.theme_tables[theme_name] or base46.theme_tables[theme_name].type ~= vim.o.background then
 	local builtin = vim.deepcopy(assert(base46.get_builtin_theme(theme_base)))
-	local harmonized = base46.theme_harmonize(builtin, "#e4301f", harmony)
+	local harmonized = base46.theme_harmonize(builtin, "#5d7cb7", harmony)
 	if settings.matugenTemplateNeovimSetBackground ~= false then
-		harmonized = base46.theme_set_bg(harmonized, "#1a1110")
+		harmonized = base46.theme_set_bg(harmonized, "#111318")
 	end
 
 	base46.theme_tables[theme_name] = harmonized
